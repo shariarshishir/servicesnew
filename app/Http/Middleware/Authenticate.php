bd4286fcb96500a40a34f6afd8589b84f1f2d7f0
arifmerchant <arif@merchantbay.com>
2021-11-23 17:54:02 +0600

    change redircet unauthenticate user

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -16,11 +16,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            //return route('login');
-           return 'http://dev.accounts.merchantbay.com/login';
-
-
-
+            return route('login');
         }
     }
 }

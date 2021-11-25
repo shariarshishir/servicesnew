0045120bfeff7e5eee65fba79207052a47993e34
shariarshishir <shariar.shishir@merchantbay.com>
2021-11-25 15:22:30 +0600

    certificate pdf form submit issue fixed

--- resources/views/business_profile/_scripts.blade.php
+++ resources/views/business_profile/_scripts.blade.php
@@ -668,7 +668,7 @@
 
       success:function(response){
         var certifications=response.certifications;
-        console.log(certifications);
+        //console.log(certifications);
         var nohtml="";
         if(certifications.length >0){
             $('.certifications-block').html(nohtml);
@@ -676,10 +676,22 @@
             for(let i = 0;i <certifications.length ;i++){
                 var html='';
                 var image="{{asset('storage/')}}"+'/'+certifications[i].image;
-                html +='<div class="col m3 l3 certificate_img">';
-                html +='<a href="javascript:void(0)" data-id="'+certifications[i].id+'" class="remove-certificate"><i class="material-icons dp48">remove_circle_outline</i></a>';
-                html +='<img src="'+image+'" alt="">';
-                html +='</div>';
+                var strArray = image.split(".");
+                
+                if(strArray[4] == 'pdf'){
+                    html +='<div class="col m3 l3 certificate_img">';
+                    html +='<a href="javascript:void(0)" style="display: none;" data-id="'+certifications[i].id+'" class="remove-certificate" ><i class="material-icons dp48">remove_circle_outline</i></a>';
+                    html +='<span>'+certifications[i].title+'</span>';
+                    html +='<i class="fa fa-file-pdf-o" style="font-size:48px;color:red"></i>';
+                    html +='<br>';
+                    html +='<a href="'+image+'" data-id="'+certifications[i].id+'" class="btn" ><i class="fas fa-arrow-alt-circle-down"></i></a>';
+                    html +='</div>';
+                } else {
+                    html +='<div class="col m3 l3 certificate_img">';
+                    html +='<a href="javascript:void(0)" data-id="'+certifications[i].id+'" class="remove-certificate"><i class="material-icons dp48">remove_circle_outline</i></a>';
+                    html +='<img src="'+image+'" alt="">';
+                    html +='</div>';
+                }
                 $('.certifications-block').append(html);
             }
 
@@ -775,27 +787,6 @@
             }, function (dismiss) {
                 return false;
             })
-    });
-
-    //delete certification
-    $(document).on('click', '.download-certificate',function(e)
-    {
-    e.preventDefault();
-    var id=$(this).attr("data-id");
-    console.log(id);
-    $.ajax({
-        url: '{{ route("certification.dowload") }}',
-        type: "GET",
-        data:{id:id},
-        success:function(response)
-            {
-                var certifications=response.certifications;
-                console.log(certifications);
-               
-            }
-                
-        });                    
-                
     });
 
     function addMainBuyersDetails()

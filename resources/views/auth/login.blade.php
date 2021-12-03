48d8278781fcedad54fa94c2cc8e7998892d85d6
arpana07 <arpana@merchantbay.com>
2021-12-03 18:20:08 +0600

    Business login page updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,15 +3,73 @@
 @section('content')
 <div class="container">
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center business_profile_login">
+
+        <div class="row">
+            <div class="col m6 registration-block">
+                <div class="company-logo">
+                    <img src="http://127.0.0.1:8000/images/frontendimages/merchantbay_logoX200.png" alt="Merchant Bay Logo">
+                </div>
+                <div class="registration-content">
+                    <p>Not Yet Registered ?</p>
+                    
+                    <a href="/?flag=service"> Click here to Register</a>
+                </div>
+            </div>
+                <div class="col m6 login-block">
+                    <span class="text-danger error-text error-msg login-error-msg" style="display: none;"></span>
+                    <form method="POST" action="#">
+                        <input type="hidden" name="_token" value="DB9Z2DehvzTPsrISNbpLC0Xo8YUQSGVmQ28myQLe">                    <div class="row">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix">email</i>
+                                <input id="email_login" type="email" class="" name="email" value="" required="" autocomplete="email" autofocus="">
+                                <label for="email" class="active">E-Mail Address</label>
+                                <span class="text-danger error-text email_err"></span>
+                                                        </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="input-field col s12 password-block-wrap">
+                                <i class="material-icons prefix">lock_outline</i>
+                                <input id="password_login" type="password" class="" name="password" required="" autocomplete="current-password">
+                                <label for="password" class="active">Password</label>
+                                <span class="text-danger error-text password_err"></span>
+                                                            <a href="javascript:void(0);" id="show-password"><i class="material-icons">visibility</i></a>
+                                <a href="javascript:void(0);" id="hide-password" style="display: none;"><i class="material-icons">visibility_off</i></a>
+                            </div>
+                        </div>
+
+                        <div class="row remember-me-block">
+                            <div class="input-field col s12">
+                                <label>
+                                    <input type="checkbox" name="remember" id="remember">
+                                    <span>Remember Me</span>
+                                </label>
+                            </div>
+                        </div>
+
+                        <button class="btn green waves-effect waves-light right signin" type="submit" name="log-in">
+                            Sign In <i class="material-icons right">send</i>
+                        </button>
+                            <a class="btn green right btn-forgot-password" href="http://127.0.0.1:8000/password/reset">
+                                Forgot Password?
+                            </a>
+                            </form>
+            </div>
+        </div>
+
+
+
+
+
+
+
+        <!-- <div class="col-md-8">
             <div class="card card-with-padding">
                 <div class="card-header">{{ __('Login') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -25,7 +83,6 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -39,14 +96,12 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="row remember-me-block">
                             <label>
                                 <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                 <span>Remember Me</span>
                             </label>
                         </div>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn waves-effect waves-light green">
@@ -63,7 +118,11 @@
                     </form>
                 </div>
             </div>
-        </div>
+        </div> -->
+
+
+
+
     </div>
 </div>
 @endsection

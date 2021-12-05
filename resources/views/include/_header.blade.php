f596b946ec969bd337dc9ea46254560a4be4c676
arpana07 <arpana@merchantbay.com>
2021-12-05 10:44:56 +0600

    Style Improvement

--- resources/views/include/_header.blade.php
+++ resources/views/include/_header.blade.php
@@ -169,8 +169,106 @@
 					<a href="javascript:void(0);" data-target="slide-out" class="sidenav-trigger waves-effect waves-block waves-light btn green lighten-1 btn-floating btn-sidenav-left"><i class="material-icons">menu</i></a>
 				</div>
 				<div class="col s6 right-align mobile_top_right">
-					<span class="material-icons"> assignment_ind </span>
-					<span class="material-icons">login</span>
+					
+					<a href="{{route('business.profile.create')}}" type="button" class="btn_profile_mobile">
+						<span class="material-icons"> assignment_ind </span>
+					</a>
+
+					<div class="cart-icon-outer-wrapper cart-icon-outer-wrapper-mobile">
+                        <div class="cart-icon-wrapper cart-icon-wrapper-mobile">
+                            <a href="javascript:void(0);" class="cart-btn_mobile">
+                                <i class="material-icons dp48">shopping_cart</i>
+                                <span id="cartItems"class="cart_counter">{{$cartItems}}</span>
+                            </a>
+                            <ul id="cart-dropdown" class="card" style="display: none;">
+                                {{-- @if(Cart::content()->count() > 0) --}}
+
+                                <li tabindex="0">
+                                    <a class="grey-text text-darken-1" href="{{route('cart.index')}}"><i class="material-icons">shopping_basket</i> My Cart</a>
+                                </li>
+                                <li tabindex="0">
+                                    <a class="grey-text text-darken-1" href="{{route('cart.destroy')}}"><i class="material-icons">delete</i> Delete all cart item</a>
+                                </li>
+                                {{-- @else
+                                <li tabindex="0">
+                                    <a class="grey-text text-darken-1" href="javascript:void(0);"><i class="material-icons">shopping_basket</i> Basket is empty</a>
+                                </li>
+                                @endif --}}
+                            </ul>
+                        </div>
+                    </div>
+					
+					
+					<div class="user-block user-block_mobile">
+						@if(env('APP_ENV') == 'production')
+							@if(Auth::guard('web')->check() && Cookie::has('sso_token'))
+								<a href="javascript:void(0);" class="dropdown-trigger waves-effect waves-block waves-light" data-target="profile-dropdown">
+									<span class="avatar-status avatar-online">
+										<img src="{{ asset('storage/'.auth()->user()->image) }}" alt="avatar">
+									</span>
+								</a>
+								<ul id="profile-dropdown" class="dropdown-content">
+									<li tabindex="0">
+										<a class="grey-text text-darken-1" href="{{ route('users.profile') }}"><i class="material-icons">person_outline</i> Profile</a>
+									</li>
+									<li tabindex="0">
+										<a class="grey-text text-darken-1" href="{{route('business.profile')}}"><i class="material-icons">store</i> My Business</a>
+									</li>
+									<li tabindex="0">
+										<a class="grey-text text-darken-1" href="{{route('wishlist.index')}}"><i class="material-icons">favorite</i> My favorite</a>
+									</li>
+									<li tabindex="0">
+										<a class="grey-text text-darken-1" href="javascript:void(0);"><i class="material-icons">settings</i> Settings</a>
+									</li>
+									<li tabindex="0">
+										<a class="grey-text text-darken-1" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="material-icons">keyboard_tab</i> Logout</a>
+									</li>
+								</ul>
+								<form id="logout-form" action="{{ route('users.logout') }}" method="POST">
+									@csrf
+								</form>
+							@else
+								<a href="#login-register-modal" class="btn_logRegi modal-trigger"><span class="material-icons">login</span></a>
+							@endif
+
+							@else
+								@if(Auth::guard('web')->check())
+								<a href="javascript:void(0);" class="dropdown-trigger waves-effect waves-block waves-light" data-target="profile-dropdown">
+									<span class="avatar-status avatar-online">
+										<img src="{{ asset('storage/'.auth()->user()->image) }}" alt="avatar">
+									</span>
+								</a>
+								<ul id="profile-dropdown" class="dropdown-content">
+									<li tabindex="0">
+										<a class="grey-text text-darken-1" href="{{ route('users.profile') }}"><i class="material-icons">person_outline</i> Profile</a>
+									</li>
+									<li tabindex="0">
+										<a class="grey-text text-darken-1" href="{{route('business.profile')}}"><i class="material-icons">store</i> My Business</a>
+									</li>
+									<li tabindex="0">
+										<a class="grey-text text-darken-1" href="{{route('wishlist.index')}}"><i class="material-icons">favorite</i> My favorite</a>
+									</li>
+									<li tabindex="0">
+										<a class="grey-text text-darken-1" href="javascript:void(0);"><i class="material-icons">settings</i> Settings</a>
+									</li>
+									<li tabindex="0">
+										<a class="grey-text text-darken-1" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="material-icons">keyboard_tab</i> Logout</a>
+									</li>
+								</ul>
+								<form id="logout-form" action="{{ route('users.logout') }}" method="POST">
+									@csrf
+								</form>
+								@else
+									<a href="#login-register-modal" class="btn_logRegi modal-trigger"><span class="material-icons">login</span></a>
+							@endif
+
+						@endif
+					</div>
+
+
+
+
+
 				</div>
 			</div>
 		</div>

4e528584362c75cf935dee263dab44db9b444779
arpana07 <arpana@merchantbay.com>
2021-12-15 18:21:08 +0600

    Style Improvement

--- resources/views/shoplanding.blade.php
+++ resources/views/shoplanding.blade.php
@@ -264,7 +264,7 @@
                             <p class="homepgp-sm mb30">Open a Digital Profile on Merchant Bay to be on the niche Search Engine of Bangladesh manufacturing industry and get Verified to be noticed and win trust of buyers.</P>
                         </div>
                         <div class="clear"></div>
-                        <img src="{{asset('images/homepage/buyers-tab-img.png')}}" alt="" class="img-responsive">
+                        <img src="{{asset('images/homepage/profile_mockup.png')}}" alt="" class="img-responsive">
                         <div class="clear20"></div>
                     </div>
                     <div class="col s12 m12 l8 plr0" id="buyercontent" style="display: none;">
@@ -273,7 +273,7 @@
                             <p class="homepgp-sm mb30">Send your inquiry through RFQ (Request For Quotation) and let our smart matching system find the best matched suppliers to quote you, along with instant quotation you will have the advantage of connecting to Verified Suppliers, Order Management Dashboard and Merchandising Assistance.</P>
                         </div>
                         <div class="clear"></div>
-                        <img src="{{asset('images/homepage/supplier-tab.png')}}" alt="" class="img-responsive">
+                        <img src="{{asset('images/homepage/profile_mockup.png')}}" alt="" class="img-responsive">
                         <div class="clear20"></div>
                     </div>
                     <!-- <div class="col-md-8 plr0" id="normalcontent">
@@ -308,7 +308,7 @@
                         <div class="clear30"></div>
                         <p class="homepgp-sm"><b style="color:#217156; font-size:20px;">Third Party Verification<br></b></P>
                         <p class="homepgp-sm mb15">Know how we verify our suppliers</P>
-                        <img src="{{asset('images/homepage/Thirdparty-verification.jpg')}}" alt="" class="img-responsive">
+                            <img src="{{asset('images/homepage/Thirdparty-verification.jpg')}}" alt="" class="img-responsive">
                         <div class="clear20"></div>
                         <div class="col s12 m10 col-md-offset-1 perform_tab_table">
                             <table class="table table-borderless ">
@@ -426,7 +426,7 @@
                         <p class="homepgp-sm mb15">
                             With our Smart Order Management Dashboard you can
                         </P>
-                        <img src="{{asset('images/homepage/dashboard.jpg')}}" alt="" class="img-responsive">
+                        <img src="{{asset('images/homepage/omd_on_mac.png')}}" alt="" class="img-responsive">
 
                         <div class="clear10"></div>
                         <div class="col m10 s12 col-md-offset-1 perform_tab_table">

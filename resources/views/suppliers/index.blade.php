64a27c771409deaac719544d62b1329104fa43ea
arpana07 <arpana@merchantbay.com>
2021-12-20 17:27:51 +0600

    Style Update

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -20,102 +20,132 @@
         ];
 @endphp
 
-    <div class="suppliers_container row">
-        <div class="col s12 m4 l3 suppliers_filter_list">
-            <form action="{{route('suppliers')}}" method="get">
-                {{--location search  --}}
-                <div class="filter_search">
-                    <i class="material-icons">search</i>
-                    <input class="filter_search_input typeahead" type="text" name="location" placeholder="Type any location" value="{{$location}}">
-                    <input class="btn_green btn_search" type="submit" value="search" onclick="this.form.submit();">
-                </div>                
-                {{-- industry_type --}}
-                <div class="filter_box">
-                    <legend>Industry Type</legend>
-                    <p>
-                        <label>
-                            <input type="checkbox" value="apparel"  name="industry_type[]" {{ (in_array('apparel', $industry_type))?'checked':'' }} onclick="this.form.submit();"/>
-                            <span>Apparel</span>
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                        <input type="checkbox" value="non-apparel" name="industry_type[]" {{ (in_array('non-apparel', $industry_type))?'checked':'' }} onclick="this.form.submit();"/>
-                            <span>Non-Apparel</span>
-                        </label>
-                    </p>
-                </div>
-                {{-- factory type --}}
-                <div class="filter_box">
-                    <legend>Factory Type</legend>
-                    @foreach ($factory_type_array as $key => $list)
-                    <p>
-                        <label>
-                            <input type="checkbox" value="{{$key}}"  name="factory_type[]" {{ (in_array($key, $factory_type))?'checked':'' }} onclick="this.form.submit();"/>
-                            <span>{{ucwords($list)}}</span>
-                        </label>
-                    </p>
-                    @endforeach
-                </div>
-                <a class='btn btn_green btn_clear' href="{{route('suppliers')}}"> Clear </a>
-            </form>
-        </div>
-        <div class="col s12 m8 m9 suppliers_filter_content">
-            {{-- business name search --}}
-            <div class="filter_search">
+    <div class="suppliers_container suppliers_filter_wrapper row">
+        <div class="col s12 m4 l3">
+            <div class="suppliers_filter_list">
+                <h3>Filter by</h3>
                 <form action="{{route('suppliers')}}" method="get">
-                    <i class="material-icons">search</i>
-                    <input class="filter_search_input" type="text" name="business_name" placeholder="business name" value="{{$business_name}}">
-                    <input class="btn_green btn_search" type="submit" value="search" onclick="this.form.submit();">
+                    {{--location search  --}}
+                    <div class="filter_search">
+                        <h4>Location</h4>
+                        <div class="filter_search_inputbox">
+                            <i class="material-icons">pin_drop</i>
+                            <input class="filter_search_input typeahead" type="text" name="location" placeholder="Type any location" value="{{$location}}">
+                        </div>
+                    
+                        <!-- <input class="btn_green btn_search" type="submit" value="search" onclick="this.form.submit();"> -->
+                    </div>                
+                    {{-- industry_type --}}
+                    <div class="filter_box">
+                        <h4>Industry Type</h4>
+                        <p>
+                            <label>
+                                <input class="btn_radio" type="checkbox" value="apparel"  name="industry_type[]" {{ (in_array('apparel', $industry_type))?'checked':'' }} onclick="this.form.submit();"/>
+                                <span>Apparel</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                            <input class="btn_radio" type="checkbox" value="non-apparel" name="industry_type[]" {{ (in_array('non-apparel', $industry_type))?'checked':'' }} onclick="this.form.submit();"/>
+                                <span>Non-Apparel</span>
+                            </label>
+                        </p>
+                    </div>
+                    {{-- factory type --}}
+                    <div class="filter_box">
+                        <h4>Factory Type</h4>
+                        @foreach ($factory_type_array as $key => $list)
+                        <p>
+                            <label>
+                                <input class="btn_radio" type="checkbox" value="{{$key}}"  name="factory_type[]" {{ (in_array($key, $factory_type))?'checked':'' }} onclick="this.form.submit();"/>
+                                <span>{{ucwords($list)}}</span>
+                            </label>
+                        </p>
+                        @endforeach
+                    </div>
+                    <a class='btn_green btn_clear' href="{{route('suppliers')}}"> Reset </a>
                 </form>
-            </div> 
-            @foreach ($suppliers as $supplier)
-                @php
-                    $mainProductsJson = json_decode($supplier->companyOverview['data']);
-                @endphp
-                <div class="industry_infoBox">
-                    <div class="box_shadow">
-                        <div class="row">
-                            <div class="supplier_profile_image_block col m3">
-                                @if($supplier->user->image)
-                                <img src="{{ asset('storage/'.$supplier->user->image) }}" alt="">
-                                @else
-                                <img src="{{asset('images/frontendimages/no-image.png')}}" alt="avatar">
-                                @endif
-
-                                @if(Auth::guard('web')->check())
-                                    <a href="{{route('supplier.profile', $supplier->id)}}">Visit Profile</a>
-                                @else
-                                    <a href="#supplier-view-auth-check-modal" class="modal-trigger">Visit Profile</button>
-                                @endif
+            </div>
+            
+        </div>
+        <div class="col s12 m8 l9">
+            <div class="suppliers_filter_content">
+                {{-- business name search --}}
+                <div class="filter_search row">
+                    <div class="col s12">
+                        <form action="{{route('suppliers')}}" method="get">
+                            <div class="filter_search_inputbox">
+                                <i class="material-icons">search</i>
+                                <input class="filter_search_input " type="text" name="business_name" placeholder="business name" value="{{$business_name}}">
+                                <input class="btn_green btn_search" type="submit" value="search" onclick="this.form.submit();">
                             </div>
-                            <div class="supplier_profile_short_info_block col m9">
-                                <h5>{{$supplier->business_name}}</h5>
-                                <p class="industry_location">Location: {{$supplier->location}}</p>
-                                <p class="industry_type">Industry Type: {{$supplier->industry_type}}</p>
-                                <p class="factory_type">Factory Type: {{$supplier->businessCategory ? $supplier->businessCategory->name : ''}}</p>
-                                @foreach($mainProductsJson as $mainProducts)
-                                    @if($mainProducts->name == 'main_products')
-                                    <p class="main_products">Main Products: {{$mainProducts->value}}</p>
+                        </form>
+                    </div>
+                </div> 
+                @foreach ($suppliers as $supplier)
+                    @php
+                        $mainProductsJson = json_decode($supplier->companyOverview['data']);
+                    @endphp
+                    <div class="industry_infoBox">
+                        <div class="industry_info_inner_box">
+                            <div class="row">
+                                <div class="supplier_profile_image_block col s12 m12 l3">
+                                    @if($supplier->user->image)
+                                    <img src="{{ asset('storage/'.$supplier->user->image) }}" alt="">
+                                    @else
+                                    <img src="{{asset('images/frontendimages/no-image.png')}}" alt="avatar">
                                     @endif
-                                @endforeach
-                            </div>                            
+
+                                    @if(Auth::guard('web')->check())
+                                        <a href="{{route('supplier.profile', $supplier->id)}}">Visit Profile</a>
+                                    @else
+                                        <a href="#supplier-view-auth-check-modal" class="modal-trigger">Visit Profile</button>
+                                    @endif
+                                </div>
+                                <div class="supplier_profile_short_info_block col s12 m12 l9">
+                                    <h5>{{$supplier->business_name}}</h5>
+                                    <div class="industry_location short_info_box"><span class="title_label">Location:</span> <span class="info_details">{{$supplier->location}}</span></div>
+                                    <div class="industry_type short_info_box"><span class="title_label">Industry Type: </span> <span class="info_details">{{$supplier->industry_type}}</span></div>
+                                    <div class="factory_type short_info_box"><span class="title_label">Factory Type:</span> <span class="info_details">{{$supplier->businessCategory ? $supplier->businessCategory->name : ''}}</span></div>
+                                    @foreach($mainProductsJson as $mainProducts)
+                                        @if($mainProducts->name == 'main_products')
+                                        <div class="main_products short_info_box"><span class="title_label">Main Products:</span> <span class="info_details">{{$mainProducts->value}}</span></div>
+                                        @endif
+                                    @endforeach
+                                </div>                            
+                            </div>
                         </div>
                     </div>
+                @endforeach
+                <div>
+                    {{$suppliers->appends(request()->query())->links()}}
                 </div>
-            @endforeach
-            <div>
-                {{$suppliers->appends(request()->query())->links()}}
             </div>
-
         </div>
     </div>
 
     <div id="supplier-view-auth-check-modal" class="modal">
         <div class="modal-content">
-            <p>Become a verified buyer to view company profile</p>
+
+            <div class="row">
+                <div class="col s12 m5 l5">
+                    <div class="supplier_view_left">
+                        <img src="http://127.0.0.1:8000/images/frontendimages/merchantbay_logoX200.png" alt="Merchant Bay Logo">
+                    </div>
+                </div>
+                <div class="col s12 m7 l7 ">
+                    <div class="supplier_view_right center-align">
+                        <h5>Become a verified buyer to view company profile</h5>
+                        <a class="btn_green" href="{{route('login')}}">sign in</a>
+                        <a class="btn_green" href="{{env('SSO_REGISTRATION_URL').'/?flag=global'}}" > sign up</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- <p>Become a verified buyer to view company profile</p>
             <a href="{{route('login')}}">sign in</a>
-            <a href="{{env('SSO_REGISTRATION_URL').'/?flag=global'}}" > sign up</a>
+            <a href="{{env('SSO_REGISTRATION_URL').'/?flag=global'}}" > sign up</a> -->
         </div>
         <div class="modal-footer">
             <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>

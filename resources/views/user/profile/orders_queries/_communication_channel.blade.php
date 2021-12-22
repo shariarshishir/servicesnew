69b10416d51de3590fdee43ad1ffe4bd7dff0a06
arpana07 <arpana@merchantbay.com>
2021-12-22 13:06:03 +0600

    Style Update

--- resources/views/user/profile/orders_queries/_communication_channel.blade.php
+++ resources/views/user/profile/orders_queries/_communication_channel.blade.php
@@ -1,4 +1,4 @@
-<div id="order-query-communication-modal" class="modal modal-fixed-footer">
+<div id="order-query-communication-modal" class="modal profile_form_modal">
     <div class="modal-content">
         <legend>Message</legend>
         <div id="pmr-errors"></div>
@@ -13,23 +13,46 @@
                 <div class="order-query-message-content">
                     <div class="row">
                         <div class="input-field col s12">
-                            <textarea class="materialize-textarea product-modification-message" name="details"></textarea>
-                            <label for="message" class="">Type your message</label>
+                            <div class="col s12 m12 l3">
+                                <label for="message" class="">Type your message </label>
+                            </div>
+                            <div class="col s12 m12 l9">
+                                <textarea class="materialize-textarea product-modification-message" name="details" placeholder="Type your message"></textarea>
+                            </div>
                         </div>
                         <div class="input-field col s12">
                             <input type="file" name="image">
                         </div>
                     </div>
+
+                    <!-- <div class="row">
+                        <div class="input-field col s12">
+                            <label for="message" class="">Type your message </label>
+                            <textarea class="materialize-textarea product-modification-message" name="details" placeholder="Type your message"></textarea>
+                        </div>
+                        <div class="input-field col s12">
+                            <input type="file" name="image">
+                        </div>
+                    </div> -->
                 </div>
                <input type="hidden" name="order_modification_request_id" value="">
-               <button type="submit" class="btn green waves-effect waves-light" id="submit-order-query-message-form">Submit</button>
+               
             </form>
+
+            <div class="submit_btn_wrap">
+                <div class="row">
+                    <div class="col s12 m6 l4 left-align"><a href="#!" class="modal-close btn_grBorder">Cancel</a></div>
+                    <div class="col s12 m6 l8 right-align">
+                        <button type="submit" class="btn_green waves-effect waves-light" id="submit-order-query-message-form">Submit</button>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
-    <div class="modal-footer">
+    <!-- <div class="modal-footer">
         <a href="javascript:void(0);" class="modal-action modal-close waves-effect waves-green btn-flat">
             <i class="material-icons green-text text-darken-1">close</i>
         </a>
-    </div>
+    </div> -->
 </div>

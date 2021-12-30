74087d15e2c521b79dbd3f75553307f66479b9db
shariarshishir <shariar.shishir@merchantbay.com>
2021-12-30 10:30:15 +0600

    Backend dashboard - improvement

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -9,6 +9,9 @@ use App\Models\Vendor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Auth;
+use DateTime;
+use DateTimeZone;
+use DateInterval;
 
 
 class AdminController extends Controller
@@ -54,6 +57,234 @@ class AdminController extends Controller
             return redirect()->route('admin.showLoginForm')->with('message','Admin has been logged out!');;
         }
 
+    public function monthlyRegisteredUsers(Request $request)
+    {
+        //dd($request->all());
+		$statsYear = $request->stats_year;
+		$statsMonth = $request->stats_month;
+		$allMonth = $request->all_month;    
+        $timezone ="Asia/Dhaka";  
+        
+		$statsFrom = $request->stats_from;
+		$statsTo = $request->stats_to;  
+        
+        $monthNames = array('01' => 'Jan',
+                        '02' => 'Feb',
+                        '03' => 'Mar',
+                        '04' => 'Apr',
+                        '05' => 'May',
+                        '06' => 'Jun',
+                        '07' => 'Jul',
+                        '08' => 'Aug',
+                        '09' => 'Sep',
+                        '10' => 'Oct',
+                        '11' => 'Nov',
+                        '12' => 'Dec',
+                    );
 
+        $totalUsers = 0;
+        $barCategories = $barData = array();
+        
+        if($statsFrom!="" && $statsTo!="")
+        {
+            $rangeStart = $statsFrom;
+            $rangeEnd = $statsTo;
+        }
+        else 
+        {
+            if($allMonth==1){ // All month
+                //print $statsMonth;
+                $tmp = explode(',', $statsMonth);
+                
+                $d = new DateTime($statsYear.$tmp[0].'01', new DateTimeZone($timezone));
+                $rangeStart = $d->format('Y-m-d');
+                
+                $d = new DateTime($statsYear.$tmp[count($tmp)-1].'01', new DateTimeZone($timezone));
+                $rangeEnd = $d->format('Y-m-t');
+            }
+            else {
+                $d = new DateTime($statsYear.$statsMonth.'01', new DateTimeZone($timezone));
+                $rangeStart = $d->format('Y-m-d');
+                $rangeEnd = $d->format('Y-m-t');
+            }
+        }
+        
+        if($allMonth==0)
+        {
+            $oStart = new DateTime($rangeStart, new DateTimeZone($timezone));
+            $oEnd = clone $oStart;
+            $oEnd->add(new DateInterval("P1M"));
+            
+            while ($oStart->getTimestamp() < $oEnd->getTimestamp()) {
+                $k = $oStart->format('Ymd');
+                $barData[$k] = 0;
+                $barCategories[] = $oStart->format('M d');
+                $oStart->add(new DateInterval("P1D"));
+            }
+        }
+        
+        $rows = User::get();
+        //dd($rows);
+        $totalUsers = count($rows);
+        
+        if(!empty($rows))
+        {
+            foreach($rows as $row)
+            {
+                if($allMonth==1)
+                {
+                    $tmp = explode(',', $statsMonth);
+                    //dd($row);
+                    $tmpDateObj = new DateTime($row->created_at, new DateTimeZone($timezone));
+                    
+                    foreach($tmp as $v)
+                    {
+                        if($tmpDateObj->format('m')==$v )
+                        {
+                            if(isset($barData[$v])){
+                                $tmp = $barData[$v];
+                                $barData[$v] = $tmp+1;
+                            }
+                            else {
+                                $barData[$v] = 1;
+                            }
+                        }
+                        if(!isset($barData[$v])){
+                            $barData[$v] = 0;
+                        }
+                    }
+                    if(!empty($barData))
+                    {
+                        foreach($barData as $k => $v)
+                        {
+                            $barCategories[$k] = $monthNames[$k];
+                        }
+                    }
+                }
+                else { // if a month is selected, we have to show date range specific data
+                    $tmpDateObj = new DateTime($row->created_at, new DateTimeZone($timezone));
+                    
+                    //foreach($barCategories as $k => $v){
+                        //$tmpArr = explode('-', $v);
+                        //if($tmpDateObj->format('d')>=$tmpArr[0] && $tmpDateObj->format('d')<=$tmpArr[1] ){
+                            //if(isset($barData[$v])){
+                                //$tmp = $barData[$v];
+                                //$barData[$v] = $tmp+1;
+                            //}
+                            //else {
+                                //$barData[$v] = 1;
+                            //}
+                        //}
+                        //if(!isset($barData[$v])){
+                            //$barData[$v] = 0;
+                        //}
+                    //}
+                    
+                    foreach($barData as $k => $v)
+                    {
+                        if($tmpDateObj->format('Ymd')==$k){
+                            $tmp = $v;
+                            $v = $tmp+1;
+                        }
+                        
+                        $barData[$k] = $v;
+                    }
+                }
+                
+                //echo '<pre>';
+                //print_r($barCategories);
+                //print_r($barData);
+            }
+        }
+        else {
+            if($allMonth==1)
+            {
+                $tmp = explode(',', $statsMonth);
+                
+                foreach($tmp as $v){
+                    $barData[$v] = 0;
+                }
+                if(!empty($barData)){
+                    foreach($barData as $k => $v){
+                        $barCategories[$k] = $monthNames[$k];
+                    }
+                }
+            }
+        }        
+        
+        //$json_result['error'] = 0;
+        //$json_result['barCategories'] = array_values($barCategories);
+        //$json_result['barData'] = array_values($barData);
+        //$json_result['totalUsers'] = $totalUsers;
+        //$json_result['msg'] = 'data collected successfully!';
+        
+        return response()->json(["error"=>0, "barCategories"=>array_values($barCategories), "barData"=>array_values($barData), "totalUsers"=>$totalUsers, "msg"=> "data collected successfully!"]);
+    }
+
+    public function getUsersBasedOnSelectedParams(Request $request) {
+        
+        //dd($request->all());
+
+        $selectedYear = $request->selectedYear;
+        $selectedMonth = $request->selectedMonth;
+        return response()->json(["status"=>1, "message"=>"Users list coming soon..."]);
+        /*
+        if($request->verifyVal == 1)
+        {
+            $company_overview = CompanyOverview::findOrFail($request->companyId);
+            $company_overview_data = json_decode($company_overview->data);
+            $data=[];
+            foreach($company_overview_data as $value)
+            {
+                array_push($data,['name' => $value->name, 'value' => $value->value, 'status' => "1"]);
+            }
+            $company_overview->update(['data' => $data]);  
+            
+            $businessProfileVerification = BusinessProfileVerification::where('business_profile_id',$company_overview->business_profile_id)->first();
+            if( $businessProfileVerification )
+            {
+                $businessProfileVerification->company_overview = 1;
+                $businessProfileVerification->save();
+            }
+            else{
+                $businessProfileVerification =new BusinessProfileVerification();
+                $businessProfileVerification->business_profile_id = $company_overview->business_profile_id;
+                $businessProfileVerification->company_overview = 1;
+                $businessProfileVerification->save();
+            }            
+        }
+        else 
+        {
+            $company_overview = CompanyOverview::findOrFail($request->companyId);
+            $company_overview_data = json_decode($company_overview->data);
+            $data=[];
+            foreach($company_overview_data as $value)
+            {
+                array_push($data,['name' => $value->name, 'value' => $value->value, 'status' => "0"]);
+            }
+            $company_overview->update(['data' => $data]); 
+
+            $businessProfileVerification = BusinessProfileVerification::where('business_profile_id',$company_overview->business_profile_id)->first();
+            if( $businessProfileVerification )
+            {
+                $businessProfileVerification->company_overview = 0;
+                $businessProfileVerification->save();
+            }
+            else{
+                $businessProfileVerification =new BusinessProfileVerification();
+                $businessProfileVerification->business_profile_id = $company_overview->business_profile_id;
+                $businessProfileVerification->company_overview = 0;
+                $businessProfileVerification->save();
+            }            
+        }
+
+        $businessProfile = BusinessProfile::where("id", $request->profileId)->first();
+        $businessProfile->is_business_profile_verified = $request->verifyVal;
+        $businessProfile->profile_verified_by_admin = Auth::guard('admin')->user()->id ;
+        $businessProfile->save();
+
+        return response()->json(["status"=>1, "message"=>"verified successfully"]);
+        */
+    }    
 
 }
--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -1,5 +1,9 @@
 
 @extends('layouts.admin')
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js"></script>
 @section('content')
 	<div class="content-wrapper">
 		<!-- Content Header (Page header) -->
@@ -46,11 +50,324 @@
 					</div>
 
 				</div>
+				
+				<div class="row" style="display: none;">
+					<div class="col-md-12">
+						<select name="stats_year" id="stats_year" class="statistics-list">
+							<option value="2017">2017</option>
+							<option value="2018">2018</option>
+							<option value="2019">2019</option>
+							<option value="2020">2020</option>
+							<option value="2021" selected="selected">2021</option>
+						</select>
+						<select name="stats_month" id="stats_month" class="statistics-list">
+							<option value="">All</option>
+							<option value="01">Jan</option>
+							<option value="02">Feb</option>
+							<option value="03">Mar</option>
+							<option value="04">Apr</option>
+							<option value="05">May</option>
+							<option value="06">Jun</option>
+							<option value="07">Jul</option>
+							<option value="08">Aug</option>
+							<option value="09">Sep</option>
+							<option value="10">Oct</option>
+							<option value="11">Nov</option>
+							<option value="12" selected="selected">Dec</option>
+						</select>		
+						<a href="javascript:void(0);" class="show-users-list-trigger">Show list</a>
+						<div class="modal fade" id="user-list-modal">
+							<div class="modal-dialog modal-lg" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="userListModalLabel">User List</h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+									</div>
+									<div class="modal-body">
+										<div class="selected-users-list-block"></div>
+									</div>
+								</div>
+							</div>
+						</div>						
 
 
+						<div id="registered-userschartbar-container" style="margin: 0 auto;"></div>
+					</div>
+					<div class="col-md-12">
+						<div id="active-userschartbar-container" style="margin: 0 auto;"></div>
+					</div>
+				</div>
 
 			</div>
 		</section>
 		<!-- /.content -->
 	</div>
   @endsection
+
+@push('js')
+<script>
+var bar_chart, xhr;
+$(document).ready(function () {
+
+    bar_chart = Highcharts.chart('registered-userschartbar-container', {
+		chart: {
+			type: 'area',
+			//width: $(window).width() - 100,
+			height: 400
+		},
+		title: {
+			text: 'Monthly registered user'
+		},
+		credits: {
+			enabled: false
+		},
+		legend: {
+			enabled: false
+		},
+		xAxis: {
+			categories: [],
+			crosshair: true
+		},
+		yAxis: {
+			allowDecimals: false,
+			min: 0,
+			title: {
+				text: 'Number of Users'
+			}
+		},
+		loading: {
+			labelStyle: {
+				color: '#333333'
+			},
+			style: {
+				"position": "absolute", "backgroundColor": "#ffffff", "opacity": 0.9, "textAlign": "center"
+			}
+		},
+		tooltip: {
+			headerFormat: '<span style="font-size:12px"><b>{point.key}</b></span><table>',
+			pointFormat: '<tr><td style="color:{series.color};padding:0">Users: </td>' +
+			'<td style="padding:0"><b>{point.y}</b></td></tr>',
+			footerFormat: '</table>',
+			shared: true,
+			useHTML: true
+		},
+		plotOptions: {
+			column: {
+				pointPadding: 0.2,
+				borderWidth: 0
+			}
+		},
+		series: [{
+			name: 'Months',
+			data: []
+		}]
+    });
+
+    $.fn.getStatsData = function() {
+		// if there is a previous ajax search, then we abort it and then set xhr to null
+			if( xhr != null ) {
+				xhr.abort();
+				xhr = null;
+			}
+		
+		var stats_month = $("#stats_month").val();
+		if(stats_month==""){
+			var all_month_val = 1;
+		}
+		else {
+			var all_month_val = 0;
+		}
+		
+		xhr = $.ajax({
+			type: 'get',
+			url: '{{ route("monthly.registered.users") }}',
+			data: {
+				stats_year: $("#stats_year").val(),
+				all_month: all_month_val,
+				stats_month: function(){
+					var stats_month = $("#stats_month").val();
+					if(stats_month=="")  // if month is ALL, send all month options
+					{
+						var all_months = new Array;
+						$("#stats_month option").each(function()
+						{
+							if($(this).val()!=""){
+								all_months.push($(this).val());
+							}
+						});
+						
+						return all_months;
+					}
+					else {
+						return stats_month;
+					}
+				}
+			},
+			dataType: 'json',
+			beforeSend: function(){
+				bar_chart.showLoading();
+			},
+			complete: function(){
+			},
+			success: function(response){
+				console.log(response);
+				bar_chart.hideLoading();
+				
+				if(response.error==0){
+
+					bar_chart.xAxis[0].setCategories(response.barCategories);
+					bar_chart.series[0].setData(response.barData);
+					
+					if(all_month_val==1){
+						bar_chart.xAxis[0].setTitle({text: 'Months'});
+					}
+					else {
+						bar_chart.xAxis[0].setTitle({text: 'Days'});
+					}
+
+				}
+			}
+		})  
+    }
+    
+    $.fn.getStatsData();
+	$('#stats_year, #stats_month').change(function(){
+		$.fn.getStatsData();
+    });
+
+	$(".show-users-list-trigger").click(function(){
+		var selectedYear = $("#stats_year").val();
+		var selectedMonth = $("#stats_month").val();
+		var url = '{{ route("get.userslist.basedonselectedparams") }}';
+		$.ajax({
+			method: 'get',
+			data: {selectedYear:selectedYear, selectedMonth:selectedMonth},
+			url: url,
+			beforeSend: function() {
+				//$('.loading-message').html("Please Wait.");
+				//$('#loadingProgressContainer').show();
+			},
+			success:function(data)
+			{
+				$('#user-list-modal').modal('show');
+				$(".selected-users-list-block").html(data.message);
+			}
+		});
+	})	
+	/*
+	Highcharts.chart('registered-userschartbar-container', {
+		chart: {
+			type: 'column'
+		},
+		title: {
+			text: 'Registered Users'
+		},
+		subtitle: {
+			text: 'Source: merchantbay.com'
+		},
+		xAxis: {
+			categories: [
+				'Jan',
+				'Feb',
+				'Mar',
+				'Apr',
+				'May',
+				'Jun',
+				'Jul',
+				'Aug',
+				'Sep',
+				'Oct',
+				'Nov',
+				'Dec'
+			],
+			crosshair: true
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				text: 'Rainfall (mm)'
+			}
+		},
+		tooltip: {
+			headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+			pointFormat: '<tr><td style="padding:0; line-height: normal;">{point.y:.1f} users</td></tr>',
+			footerFormat: '</table>',
+			shared: true,
+			useHTML: true,
+			style: {
+				pointerEvents: 'auto'
+			}
+		},
+		plotOptions: {
+			column: {
+				pointPadding: 0.2,
+				borderWidth: 0
+			}
+		},
+		series: [{
+			name: 'Monthly registered users',
+			data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
+		}]
+	});
+	*/
+	/*
+	Highcharts.chart('active-userschartbar-container', {
+		chart: {
+			type: 'column'
+		},
+		title: {
+			text: 'Active Users'
+		},
+		subtitle: {
+			text: 'Source: merchantbay.com'
+		},
+		xAxis: {
+			categories: [
+				'Jan',
+				'Feb',
+				'Mar',
+				'Apr',
+				'May',
+				'Jun',
+				'Jul',
+				'Aug',
+				'Sep',
+				'Oct',
+				'Nov',
+				'Dec'
+			],
+			crosshair: true
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				text: 'Rainfall (mm)'
+			}
+		},
+		tooltip: {
+			headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+			pointFormat: '<tr><td style="padding:0; line-height: normal;">{point.y:.1f} users</td></tr>',
+			footerFormat: '</table>',
+			shared: true,
+			useHTML: true,
+			style: {
+				pointerEvents: 'auto'
+			}
+		},
+		plotOptions: {
+			column: {
+				pointPadding: 0.2,
+				borderWidth: 0
+			}
+		},
+		series: [{
+			name: 'Last 2 weeks active users',
+			data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
+		}]
+	});
+	*/
+});	
+</script>
+@endpush

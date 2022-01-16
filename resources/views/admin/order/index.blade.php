fd16a010b738aff6f6df53320fece161440440d8
arpana07 <arpana@merchantbay.com>
2022-01-16 11:00:56 +0600

    Backend Improvement

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -20,67 +20,69 @@
 
     <!-- Main content -->
     <section class="content">
-    <div class="container-fluid">
+        <div class="container-fluid">
 
-	  	{{-- <div class="row" style="padding-bottom: 20px;">
-			<div class="col-lg-12">
-				<a href="{{route('vendor.order.create',$vendorId)}}" class="btn btn-success" style="display: none;"><i class="fas fa-plus"></i> Add New order</a>
-			</div>
-		</div> --}}
+            {{-- <div class="row" style="padding-bottom: 20px;">
+                <div class="col-lg-12">
+                    <a href="{{route('vendor.order.create',$vendorId)}}" class="btn btn-success" style="display: none;"><i class="fas fa-plus"></i> Add New order</a>
+                </div>
+            </div> --}}
 
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                <legend>Orders List</legend>
-                    @if(count($collection)>0)
-                    <table class="table table-bordered orders-table">
-                        <thead>
-                            <tr>
-                            <th>Sl No.</th>
-                            <th>Order No.</th>
-                            <th>Order Status</th>
-                            <th>Payment Status</th>
-                            </tr>
-                        </thead>
+            <div class="row admin_order_list_table_wrap">
+                <div class="col-md-12">
+                    <div class="card">
+                        <legend>Orders List</legend>
+                        <div class="no_more_tables">
+                            @if(count($collection)>0)
+                            <table class="table table-bordered orders-table">
+                                <thead class="cf">
+                                    <tr>
+                                    <th>Sl No.</th>
+                                    <th>Order No.</th>
+                                    <th>Order Status</th>
+                                    <th>Payment Status</th>
+                                    </tr>
+                                </thead>
 
-                        <tbody>
-                            @foreach($collection as $key=>$list)
-                                <tr>
-                                    <td>
-                                        {{$key+1}}
-                                    </td>
-                                    <td>
-                                        <a href="{{route('business.profile.order.show',['business_profile_id' => $list->business_profile_id,'order_id'=>$list->id])}}">{{$list->order_number}}</a>
-                                    </td>
-                                    <td class="@switch($list->state)
-                                        @case('pending')
-                                            text-danger
-                                            @break
-                                        @case('approved')
-                                            text-success
-                                            @break
-                                        @case('delivered')
-                                            text-info
-                                            @break
-                                        @default
+                                <tbody>
+                                    @foreach($collection as $key=>$list)
+                                        <tr>
+                                            <td data-title="Sl No.">
+                                                {{$key+1}}
+                                            </td>
+                                            <td data-title="Order No.">
+                                                <a href="{{route('business.profile.order.show',['business_profile_id' => $list->business_profile_id,'order_id'=>$list->id])}}">{{$list->order_number}}</a>
+                                            </td>
+                                            <td data-title="Order Status" class="@switch($list->state)
+                                                @case('pending')
+                                                    text-danger
+                                                    @break
+                                                @case('approved')
+                                                    text-success
+                                                    @break
+                                                @case('delivered')
+                                                    text-info
+                                                    @break
+                                                @default
 
-                                    @endswitch">{{ ucfirst($list->state)}}</td>
-                                    <td class="{{$list->payment_status == 'paid' ? 'text-success' : 'text-warning'}}">{{ ucfirst($list->payment_status) }}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                            @endswitch">{{ ucfirst($list->state)}}</td>
+                                            <td data-title="Payment Status" class="{{$list->payment_status == 'paid' ? 'text-success' : 'text-warning'}}">{{ ucfirst($list->payment_status) }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
 
-                    @else
-                        <div class="alert alert-info alert-dismissible">
-                            INFO : No orders available.
+                            @else
+                                <div class="alert alert-info alert-dismissible">
+                                    INFO : No orders available.
+                                </div>
+                            @endif
                         </div>
-                    @endif
+                    </div>
                 </div>
             </div>
-        </div>
 
-    </div>
+        </div>
     </section>
 </div>
 

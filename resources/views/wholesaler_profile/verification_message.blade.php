8a01061955b7f19037a657746e1fa22844998a97
shariarshishir <shariar.shishir@merchantbay.com>
2022-01-24 17:35:47 +0600

    Business profile verfication message in profile

---
+++ resources/views/wholesaler_profile/verification_message.blade.php
@@ -0,0 +1,7 @@
+<div class="profile_verification_request_block">
+    <div class="card-alert card orange">
+        <div class="card-content white-text">
+            <p>WARNING : Your profile is not verified. <a href="#send-verification-request-modal" class="send-verification-request-trigger modal-trigger">Send Request</a></p>
+        </div>
+    </div>										
+</div>	

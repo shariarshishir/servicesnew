5cbad24a652bd5a00e3e9ab16483d42c5bbcbfb6
Asaduzzaman Rabbi <asad@merchantbay.com>
2022-01-25 16:04:43 +0600

    business profile verification message improvment in wholesaler end

---
+++ resources/views/business_profile/_create_or_update_business_profile_verification_request.blade.php
@@ -0,0 +1,18 @@
+
+<div id="send-verification-request-modal" class="modal">
+    <div class="modal-content">
+        <legend>Request for verification</legend>
+        <form id="profile_verification_request" method="POST">
+            <div class="row">
+                <div class="input-field col s12">
+                    <label for="verification_message">Message <i data-position="top" data-tooltip="Write your message below" class="material-icons tooltipped">info</i></label>
+                    <textarea id="verification_message" name="verification_message" value=""></textarea>
+                </div>
+            </div>
+            <input type="hidden" id="requested_business_profile_id" name="requested_business_profile_id" value="{{$business_profile->id}}" />
+			<input type="hidden" id="requested_business_profile_name" name="requested_business_profile_name" value="{{$business_profile->business_name}}" />
+            <button type="button" class="modal-action modal-close waves-effect waves-green btn btn_green">Close</button>            
+            <button type="button" class="btn btn_green waves-effect waves-green verification_request_trigger">Submit <i class="material-icons right">send</i></button>
+        </form>
+    </div>
+</div>
--- resources/views/business_profile/show.blade.php
+++ resources/views/business_profile/show.blade.php
@@ -110,7 +110,7 @@
 						</ul>
 					</div>
 					@if($business_profile->is_business_profile_verified == 0)
-					<div class="profile_verification_request_block" style="display: none;">
+					<div class="profile_verification_request_block">
 						<div class="card-alert card orange">
 							<div class="card-content white-text">
 								<p>WARNING : Your profile is not verified. <a href="#send-verification-request-modal" class="send-verification-request-trigger modal-trigger">Send Request</a></p>
@@ -1736,23 +1736,6 @@
 	</div>
 </section>
 
-<div id="send-verification-request-modal" class="modal">
-    <div class="modal-content">
-        <legend>Request for verification</legend>
-        <form id="profile_verification_request" method="POST">
-            <div class="row">
-                <div class="input-field col s12">
-                    <label for="verification_message">Message <i data-position="top" data-tooltip="Write your message below" class="material-icons tooltipped">info</i></label>
-                    <textarea id="verification_message" name="verification_message" value=""></textarea>
-                </div>
-            </div>
-            <input type="hidden" id="requested_business_profile_id" name="requested_business_profile_id" value="{{$business_profile->id}}" />
-			<input type="hidden" id="requested_business_profile_name" name="requested_business_profile_name" value="{{$business_profile->business_name}}" />
-            <button type="button" class="modal-action modal-close waves-effect waves-green btn btn_green">Close</button>            
-            <button type="button" class="btn btn_green waves-effect waves-green verification_request_trigger">Submit <i class="material-icons right">send</i></button>
-        </form>
-    </div>
-</div>
 
     @include('business_profile._edit_company_overview_modal')
     @include('business_profile._edit_capacity_and_machineries_modal')
@@ -1775,6 +1758,7 @@
 	@include('business_profile._create_or_update_terms_of_service_modal')
     @include('business_profile._edit_categories_produced')
     @include('business_profile._edit_machinery_details')
+	@include('business_profile._create_or_update_business_profile_verification_request')
 @endsection
 
 @include('business_profile._scripts')

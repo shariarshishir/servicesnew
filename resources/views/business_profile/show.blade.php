52326b58db28453d96207f4711b1915f93ab2440
shariarshishir <shariar.shishir@merchantbay.com>
2022-01-27 16:52:22 +0600

    Business profile - improvement

--- resources/views/business_profile/show.blade.php
+++ resources/views/business_profile/show.blade.php
@@ -111,17 +111,19 @@
 					</div>
 					@if($business_profile->is_business_profile_verified == 0)
 					<div class="profile_verification_request_block">
-						<div class="card-alert card orange">
-							@if($business_profile->businessProfileVerificationsRequest)
-							<div class="card-content white-text">
-								<p>WARNING : Your request is awaiting for verification. <a href="#send-verification-request-modal" class="send-verification-request-trigger modal-trigger">Resend Request</a></p>
+						@if($business_profile->businessProfileVerificationsRequest)
+						<div class="card-alert card orange lighten-5">
+							<div class="card-content orange-text">
+								<p><i class="material-icons verification-info-icon">info_outline</i> Your request is awaiting for verification. <a href="#send-verification-request-modal" class="send-verification-request-trigger modal-trigger">Resend Request</a></p>
 							</div>
-							@else
-							<div class="card-content white-text">
-								<p>WARNING : Your profile is not verified. <a href="#send-verification-request-modal" class="send-verification-request-trigger modal-trigger">Send Request</a></p>
+						</div>
+						@else
+						<div class="card-alert card orange lighten-5">
+							<div class="card-content orange-text">
+								<p><i class="material-icons verification-info-icon">info_outline</i> Your profile is not verified. <a href="#send-verification-request-modal" class="send-verification-request-trigger modal-trigger">Send Request</a></p>
 							</div>
-							@endif
-						</div>										
+						</div>
+						@endif
 					</div>	
 					@endif					
 					<div id="home" class="tabcontent">
--- resources/views/wholesaler_profile/verification_message.blade.php
+++ resources/views/wholesaler_profile/verification_message.blade.php
@@ -1,13 +1,15 @@
 <div class="profile_verification_request_block">
-    <div class="card-alert card orange">
-        @if($business_profile->businessProfileVerificationsRequest)
-        <div class="card-content white-text">
-            <p>WARNING : Your request is awaiting for verification. <a href="#send-verification-request-modal" class="send-verification-request-trigger modal-trigger">Resend Request</a></p>
+    @if($business_profile->businessProfileVerificationsRequest)
+    <div class="card-alert card orange lighten-5">
+        <div class="card-content orange-text">
+            <p><i class="material-icons verification-info-icon">info_outline</i> Your request is awaiting for verification. <a href="#send-verification-request-modal" class="send-verification-request-trigger modal-trigger">Resend Request</a></p>
         </div>
-        @else
-        <div class="card-content white-text">
-            <p>WARNING : Your profile is not verified. <a href="#send-verification-request-modal" class="send-verification-request-trigger modal-trigger">Send Request</a></p>
+    </div>
+    @else
+    <div class="card-alert card orange lighten-5">
+        <div class="card-content orange-text">
+            <p><i class="material-icons verification-info-icon">info_outline</i> Your profile is not verified. <a href="#send-verification-request-modal" class="send-verification-request-trigger modal-trigger">Send Request</a></p>
         </div>
-        @endif
-    </div>										
+    </div>
+    @endif
 </div>	

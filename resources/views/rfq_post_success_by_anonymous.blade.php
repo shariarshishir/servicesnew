18dd3ef1a31c929afb3ae1adb9337138b40669ce
arpana <arpana@merchantbay.com>
2022-07-06 16:12:55 +0600

    Style Update

--- resources/views/rfq_post_success_by_anonymous.blade.php
+++ resources/views/rfq_post_success_by_anonymous.blade.php
@@ -4,8 +4,10 @@
 
 <div class="row">
     <div class="col m12">
-        <h2>Congratulations. Your RFQ have been successfully.</h2>
-        <p>Merchantbay will review your RFQ and will get back you within 24 hours.</p>
+        <div class="rfq_post_success">
+            <h2><span>Congratulations.</span> Your RFQ have been successfully.</h2>
+            <p>Merchantbay will review your RFQ and will get back you within 24 hours.</p>
+        </div>
     </div>
 </div>
 
